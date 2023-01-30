9c3d392ea92615421c146c876ecdb1f809694ef8
dv <dvaug86@gmail.com>
2023-01-30 13:20:03 -0500

    connection to contentful made

---
+++ components/HomePage.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import Link from "next/link";
+// const HomePage = () => {
+//   return (
+//     <div>
+//       <div></div>
+//       <button>
+//         <Link href="/gallery">Gallery</Link>
+//       </button>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
