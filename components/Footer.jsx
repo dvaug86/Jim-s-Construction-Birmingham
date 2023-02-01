b7ef06fce73c81db134e19a8449e906b65bb1638
Michael Vaughan <mgvaughan66@gmail.com>
2023-02-01 12:16:01 -0500

    adding navbar and footer

---
+++ components/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Footer = () => {
+    return (
+        <section className='bg-[#2e7d32] py-6'>
+            <div className='max-w-[1240px] justify-content mx-auto w-5/6 gap-16 md:flex'>
+
+                {/* left side contact */}
+                <div className="mt-8 basis-1/3 md:mt-0 text-white">
+                    <h1 className="my-2 font-bold text-lg">How to Reach Us</h1>
+                    <p>xxx-xxx-xxxx</p>
+                    <div>
+                        <Link href='/'>Email Us</Link>
+                    </div>
+                    <div>
+                        <Link href="https://www.facebook.com">Facebook</Link>
+                    </div>
+                </div>
+
+                {/* middle side links */}
+                <div className="mt-8 basis-1/3 md:mt-0 text-white">
+                    <h1 className='my-2 font-bold flex text-lg'>Navigate</h1>
+                    <div>
+                        <Link href='/'>Home</Link>
+                        <Link href='/'>Contact Us</Link>
+                        <Link href='/'>Services</Link>
+                        <Link href='/'>Gallery</Link>
+                        <Link href='/'>Testimonials</Link>
+                    </div>
+                </div>
+            </div>
+            {/* right side  built */}
+            <div className='max-w-[1240px] text-sm mt-4 justify-content mx-auto w-5/6 gap-16 flex text-white'>
+                <p>Built in Marietta by M&D Web Design</p>
+            </div>
+        </section>
+    )
+}
+
+export default Footer;
---
+++ components/Navbar.jsx
@@ -0,0 +1,132 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import navLogo from '../public/assets/navLogo.png'
+
+const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const [shadow, setShadow] = useState(false);
+    const [navBg, setNavBg] = useState('#ecf0f3');
+    const [linkColor, setLinkColor] = useState('#1f2937');
+
+    useEffect(() => {
+        const handleShadow = () => {
+            if (window.scrollY >= 90) {
+                setShadow(true)
+            } else {
+                setShadow(false)
+            }
+        }
+        window.addEventListener('scroll', handleShadow);
+    }, []);
+
+    const handleNav = () => {
+        setNav(!nav);
+    }
+
+
+    return (
+        <div style={{ backgroundColor: `${navBg}` }} className={shadow ? 'fixed w-full h-20 shadow-xl z-[100]' : 'fixed w-full h-20 z-[100]'}>
+            <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+                <Link href='/'>
+                    <Image
+                        src={navLogo}
+                        alt="company logo"
+                        width="75"
+                        height="50"
+                        priority
+                    />
+                </Link>
+
+                <div>
+                    <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Contact Us</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Services</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Gallery</li>
+                        </Link>
+                        <Link href='/'>
+                            <li className="ml-10 text-sm uppercase hover:border-b">Testimonials</li>
+                        </Link>
+                    </ul>
+                    <div onClick={handleNav} className='md:hidden'>
+                        <AiOutlineMenu size={25} />
+                    </div>
+                </div>
+            </div>
+
+            <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+                <div clasName={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
+                    <div className="flex w-full items-center justify-between">
+                        <Link href='/'>
+                            <Image 
+                            src={navLogo}
+                            width="60"
+                            hight="35"
+                            alt="/"
+                            />
+                        </Link>
+                        <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 cursor-pointer">
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    <div className="border-b border-gray-300 my-4">
+                        <p className="w-[85%] md:w-[90%] py-4">Let's build something together</p>
+                    </div>
+                    <div className='py-4 flex flex-col'>
+                        <ul className='uppercase'>
+                            <Link href='/'>
+                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Home</li>
+                            </Link>
+                            <Link href='/'>
+                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Contact Us</li>
+                            </Link>
+                            <Link href='/'>
+                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Services</li>
+                            </Link>
+                            <Link href='/'>
+                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Gallery</li>
+                            </Link>
+                            <Link href='/'>
+                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Testimonials</li>
+                            </Link>
+                        </ul>
+                        <div className="pt-40">
+                            <p className="uppercase tracking-widest text-[#5651e5]">Let's Connect</p>
+                            <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaLinkedinIn />
+                                </div>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <FaGithub />
+                                </div>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <AiOutlineMail />
+                                </div>
+                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
+                                    <BsFillPersonLinesFill />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+export default Navbar;
