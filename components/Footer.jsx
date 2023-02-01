1815a86c83fa199a79d409ee8fe4f711b655e1a4
Michael Vaughan <mgvaughan66@gmail.com>
2023-02-01 13:55:10 -0500

    adding additional navbar and footer styling

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <section className='bg-[#3482ad] py-6'>
-            <div className='max-w-[1240px] justify-content mx-auto w-5/6 gap-16 md:flex'>
+        <section className='bg-[#3482ad] py-6 text-white'>
+            <div className='max-w-[1240px] justify-content mx-auto w-5/6 gap-16'>
+                <div className="flex justify-center gap-4">
+                    <div>
+                        <AiFillFacebook size={30}/>
+                    </div>
+                    <div>
+                        <AiFillInstagram size={30}/>
+                    </div>
+                </div>
+                <p className="text-center my-2 font-bold">Jim's Construction, LLC</p>
+                <p className="text-center my-2 font-bold">Birmingham, Alabama</p>
+                <div className='justify-center flex gap-2 my-2 font-bold'>
+                    <p>info@jimsconstructionbham.com</p>
+                    <p>|</p>
+                    <p>205-253-9901</p>
+                </div>
+                <p className="text-center mt-4 text-sm">Copyright @ 2020 Jim's Construction, LLC - All Rights Reserved</p>
+                <p className="text-center mt-1 text-xs">Built in Marietta by M&D Web Design</p>
 
                 {/* left side contact */}
-                <div className="mt-8 basis-1/3 md:mt-0 text-white">
+                {/* <div className="mt-8 basis-1/3 md:mt-0 text-white">
                     <h1 className="my-2 font-bold text-lg">How to Reach Us</h1>
                     <p>xxx-xxx-xxxx</p>
                     <div>
@@ -16,10 +35,10 @@ const Footer = () => {
                     <div>
                         <Link href="https://www.facebook.com">Facebook</Link>
                     </div>
-                </div>
+                </div> */}
 
                 {/* middle side links */}
-                <div className="mt-8 basis-1/3 md:mt-0 text-white">
+                {/* <div className="mt-8 basis-1/3 md:mt-0 text-white">
                     <h1 className='my-2 font-bold flex text-lg'>Navigate</h1>
                     <div>
                         <Link href='/'>Home</Link>
@@ -28,11 +47,11 @@ const Footer = () => {
                         <Link href='/'>Gallery</Link>
                         <Link href='/'>Testimonials</Link>
                     </div>
-                </div>
+                </div> */}
             </div>
             {/* right side  built */}
             <div className='max-w-[1240px] text-sm mt-4 justify-content mx-auto w-5/6 gap-16 flex text-white'>
-                <p>Built in Marietta by M&D Web Design</p>
+                
             </div>
         </section>
     )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,6 +5,8 @@ import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import navLogo from '../public/assets/navLogo.png'
+import { AiFillFacebook } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai'
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -27,22 +29,23 @@ const Navbar = () => {
         window.addEventListener('scroll', handleShadow);
     }, []);
 
-    
+
 
 
     return (
         <div className={shadow ? 'fixed w-full h-20 shadow-xl z-[100] bg-[#3482ad]/90' : 'fixed w-full h-20 z-[100] bg-[#3482ad]/90'}>
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
-                <Link href='/'>
-                    <Image
-                        src={navLogo}
-                        alt="company logo"
-                        width="75"
-                        height="50"
-                        priority
-                    />
-                </Link>
-
+                <div className="h-[80%] w-[120px]">
+                    <Link href='/'>
+                        <Image
+                            src={navLogo}
+                            alt="company logo"
+                            width="120"
+                            height="50"
+                            priority
+                        />
+                    </Link>
+                </div>
                 <div>
                     <ul className='hidden md:flex text-white'>
                         <Link href='/'>
@@ -61,71 +64,62 @@ const Navbar = () => {
                             <li className="ml-10 text-sm uppercase hover:border-b">Testimonials</li>
                         </Link>
                     </ul>
-                    <div onClick={handleNav} className='md:hidden'>
+                    <div onClick={handleNav} className='md:hidden text-white'>
                         <AiOutlineMenu size={25} />
                     </div>
                 </div>
             </div>
 
             <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
-                <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
-                    <div className="flex w-full items-center justify-between">
-                        <Link href='/'>
-                            <Image 
-                            src={navLogo}
-                            width="60"
-                            hight="35"
-                            alt="/"
-                            />
-                        </Link>
-                        <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 cursor-pointer">
-                            <AiOutlineClose />
+                <div className={nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#3482ad] p-10 ease-in duration-500' : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'}>
+                    <div className="flex w-full items-center justify-end">
+
+                        <div onClick={handleNav} className="rounded-full shadow-lg text-black bg-white cursor-pointer">
+                            <AiOutlineClose size={25} />
                         </div>
                     </div>
-                    <div className="border-b border-gray-300 my-4">
-                        <p className="w-[85%] md:w-[90%] py-4">Let's build something together</p>
+                    <div className="border-b border-gray-300 my-4 w-[70%] mx-auto">
+                        <Link href='/'>
+                            <Image
+                                src={navLogo}
+                                width="auto"
+                                hight="35"
+                                alt="/"
+                            />
+                        </Link>
                     </div>
-                    <div className='py-4 flex flex-col'>
+                    <div className='py-4 flex flex-col mx-auto text-white font-bold w-[70%]'>
                         <ul className='uppercase'>
                             <Link href='/'>
-                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Home</li>
+                                <li onClick={() => setNav(false)} className='py-4 text-base'>Home</li>
                             </Link>
                             <Link href='/'>
-                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Contact Us</li>
+                                <li onClick={() => setNav(false)} className='py-4 text-base'>Contact Us</li>
                             </Link>
                             <Link href='/'>
-                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Services</li>
+                                <li onClick={() => setNav(false)} className='py-4 text-base'>Services</li>
                             </Link>
                             <Link href='/'>
-                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Gallery</li>
+                                <li onClick={() => setNav(false)} className='py-4 text-base'>Gallery</li>
                             </Link>
                             <Link href='/'>
-                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Testimonials</li>
+                                <li onClick={() => setNav(false)} className='py-4 text-base'>Testimonials</li>
                             </Link>
                         </ul>
-                        <div className="pt-40">
-                            <p className="uppercase tracking-widest text-[#5651e5]">Let's Connect</p>
-                            <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                    <FaLinkedinIn />
-                                </div>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                    <FaGithub />
-                                </div>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                    <AiOutlineMail />
-                                </div>
-                                <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300'>
-                                    <BsFillPersonLinesFill />
-                                </div>
+                    </div>
+                    <div className="flex justify-center">
 
+                        <div className="flex text-black justify-center gap-8 my-4 w-full sm:w-[80%]">
+                            <div className='rounded-full shadow-lg  p-3 cursor-pointer hover:scale-105 ease-in duration-300 bg-white'>
+                                <AiFillFacebook size={25} />
+                            </div>
+                            <div className='rounded-full shadow-lg  p-3 cursor-pointer hover:scale-105 ease-in duration-300 bg-white'>
+                                <AiFillInstagram size={25} />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
     )
 
